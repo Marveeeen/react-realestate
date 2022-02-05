9ba2bca163be85ed38bc556f64c15f698c03d65c
Marveeen <jmvroqueee@gmail.com>
2022-02-05 10:42:39 +0800

    Added the environment variables

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,8 +6,8 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': 'a8e2c852ffmshcf9b5c4d20824a8p14af2djsn9d16ce30bb5c'
-        }
+            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY
+        },
     });
 
     return data
